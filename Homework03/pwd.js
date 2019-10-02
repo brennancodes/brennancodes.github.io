721fd2902a2b97c45754f68d5011e860b861e2ea
Brennan <brennancodes@gmail.com>
2019-10-01 20:47:26 -0500

    Updated layout and basic functionality. Still non-functional.

--- Homework03/pwd.js
+++ Homework03/pwd.js
@@ -1,1 +1,37 @@
-console.log(document.getElementById("checkLow").checked)
+// checkL()
+// checkU()
+// checkN()
+// uncheckS()
+
+var lowerCase = document.getElementById("checkLow");
+var upperCase = document.getElementById("checkUp");
+var numBers = document.getElementById("checkNum");
+var specChar = document.getElementById("checkSpec");
+var charNum = document.getElementById("charNum");
+
+var generatePassword = document.getElementById("generatePassword");
+
+function checkBoo(checkBox) {
+    //console.log(checkBox.checked);
+    console.log(checkBox.value)
+}
+
+generatePassword.addEventListener("click",function(){
+    checkBoo(lowerCase);
+    checkBoo(upperCase);
+    checkBoo(numBers);
+    checkBoo(specChar);
+    checkBoo(charNum);
+})
+
+//Allow user to select number of characters by capturing value of textbox. (min 8 max 128)
+//Allow user to select from options for which characters to use.
+//Pull characters randomly from TRUE booleans (checkboxes).
+
+//Steps:
+//Define arrays or strings for the four options.
+//Create a function that checks the checkboxes.
+//Based off of truth or falsehood, add arrays or strings to a larger array, then use the new array length.
+//I can use my checkBoo function to create a variable with that array.
+//Empty string or array at top... if statements to fill the array.
+//Then, feed that array to the generatePass function with math.random stuffs.
