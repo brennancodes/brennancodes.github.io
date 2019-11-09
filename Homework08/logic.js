127622a3962912e38f36ba812870d397da7c77a1
Brennan <brennancodes@gmail.com>
2019-11-08 18:07:07 -0600

    404 Not Found

---
+++ Homework08/logic.js
@@ -0,0 +1,25 @@
+var project01URL = "https://ygajway.github.io/Project_01/"
+var passwordGenURL = "https://brennancodes.github.io/Homework03/"
+var plannerURL = "https://brennancodes.github.io/Homework05/"
+var weatherAppURL = "https://brennancodes.github.io/Homework06/"
+
+
+$("#floaty1").on("click",function(){
+    window.open(project01URL);
+});
+$("#floaty2").on("click",function(){
+    window.open(passwordGenURL);
+})
+
+$("#floaty3").on("click",function(){
+    window.open(plannerURL);
+})
+
+$("#floaty4").on("click",function(){
+    window.open(weatherAppURL);
+})
+
+// $("#floaty5").on("click",function(){
+//     window.open();
+// })
+
