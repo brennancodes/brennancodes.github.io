bd33bf4943f45a11bc585596bb25976a45cb2fc3
Brennan <brennancodes@gmail.com>
2019-11-16 14:49:09 -0600

    trying to clear up space

--- Homework09/index.js
+++ Homework09/index.js
@@ -6,58 +6,75 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
     return inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is your name?"
-        },
+        // {
+        //     type: "input",
+        //     name: "name",
+        //     message: "What is your full name?"
+        // },
         {
             type: "input",
             name: "github",
             message: "What is your github username?"
         },
-        {
-            type: "input",
-            name: "linkedin",
-            message: "What is your linkedin username?"
-        },
-        {
-            type: "input",
-            name: "color",
-            message: "What is your favorite color?"
-        }
+        // {
+        //     type: "input",
+        //     name: "linkedin",
+        //     message: "What is your linkedin username?"
+        // },
+        // {
+        //     type: "input",
+        //     name: "color",
+        //     message: "What is your favorite color?"
+        // }
     ])
 };
 
-function generateHTML(answers, data) {
+function generateHTML(data) {
     return `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <title>Document</title>
-  </head>
-  <body>
-    <div class="jumbotron jumbotron-fluid">
-    <div class="container" style="background-color=${answers.color}">
-      <img class="text-center" src=${data.avatar}>
-      <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-      <p class="lead">I am from ${data.location}.</p>
-      <h3><span class="badge badge-secondary">Contact Me</span></h3>
-      <ul class="list-group">
-        <li class="list-group-item">My GitHub username is ${answers.github}</li>
-        <li class="list-group-item">My LinkedIn username is ${answers.linkedin}</li>
-        <li class-"list-group-item">Public Repositories: ${data.repos}</li>
-        <li class-"list-group-item">Followers: ${data.followers}</li>
-        <li class-"list-group-item">GitHub Stars: ${data.stars}</li>
-        <li class-"list-group-item">Following: ${data.following}</li>
-      </ul>
-    </div>
-  </div>
-  </body>
-  </html>`;
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+            <title>Developer Profile</title>
+        </head>
+        <body>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <img class="face" src=${data.avatar}>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-12"><h2>Hello!</h2></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12"><h3>My name is</h3></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12"><h1>${data.name}</h1></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-4"><a class="location">${data.location}</a></div>
+                            <div class="col-sm-4"><a class="github" href="https://www.github.com/${data.username}">${data.username}</a></div>
+                            <div class="col-sm-4"><a class="website" href=${data.website}></a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12"><h3>${data.bio}</h3></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6"><h2>Public Repositories: ${data.repos}</h2></div>
+                    <div class="col-sm-6"><h2>Followers: ${data.followers}</h2></div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6"><h2>GitHub Stars: ${data.stars}</h2></div>
+                    <div class="col-sm-6"><h2>Following: ${data.following}</h2></div>
+                </div>
+            </div>
+        </body>`;
   }
 
 promptUser()
@@ -69,7 +86,7 @@ promptUser()
         throw err;
     }
     console.log(data); // or what ever you want to do with the data
-    const html = generateHTML(answers, data);
+    const html = generateHTML(data);
     return writeFileAsync("index.html", html);
     })
 
