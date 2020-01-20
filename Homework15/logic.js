ccebec65a3d97137933b9e82f592eed3a79c8670
Brennan <brennancodes@gmail.com>
2020-01-19 19:21:52 -0600

    prepping HW15 init commit

---
+++ Homework15/logic.js
@@ -0,0 +1,76 @@
+//Live Website Links
+var project01URL = "https://brennancodes.github.io/Homework07/"
+var passwordGenURL = "https://brennancodes.github.io/Homework03/"
+var plannerURL = "https://brennancodes.github.io/Homework05/"
+var weatherAppURL = "https://brennancodes.github.io/Homework06/"
+var linkedInURL = "https://www.linkedin.com/in/brennancodes/"
+var githubURL = "https://github.com/brennancodes"
+
+//Repository Links
+var tppURL = "https://github.com/brennancodes/brennancodes.github.io/tree/master/Homework07"
+var pwdURL = "https://github.com/brennancodes/brennancodes.github.io/tree/master/Homework03"
+var dpURL = "https://github.com/brennancodes/brennancodes.github.io/tree/master/Homework05"
+var wdURL = "https://github.com/brennancodes/brennancodes.github.io/tree/master/Homework06"
+
+//PDF Link
+var resume = "https://brennancodes.github.io/Homework08/Assets/pdf/BS_Resume.pdf"
+
+//Peace Project Links
+$(".githubtpp").on("click",function(){
+    window.open(tppURL);
+})
+$("#floaty1").on("click",function(){
+    window.open(project01URL);
+});
+$(".livetpp").on("click",function(){
+    window.open(project01URL);
+});
+
+//Password Generator Links
+$(".githubpwd").on("click",function(){
+    window.open(pwdURL);
+})
+$("#floaty2").on("click",function(){
+    window.open(passwordGenURL);
+})
+$(".livepwd").on("click",function(){
+    window.open(passwordGenURL);
+})
+
+//Daily Planner Links
+$(".githubdp").on("click",function(){
+    window.open(dpURL);
+})
+$("#floaty3").on("click",function(){
+    window.open(plannerURL);
+})
+$(".livedp").on("click",function(){
+    window.open(plannerURL);
+})
+
+//Weather Dashboard Links
+$(".githubwd").on("click",function(){
+    window.open(wdURL);
+})
+$("#floaty4").on("click",function(){
+    window.open(weatherAppURL);
+})
+$(".livewd").on("click",function(){
+    window.open(weatherAppURL);
+})
+
+
+$(".linkedIn").on("click",function(){
+    window.open(linkedInURL);
+})
+
+$(".github").on("click",function(){
+    window.open(githubURL);
+})
+$(".resumeicon").on("click",function(){
+    window.open(resume);
+})
+// $("#floaty5").on("click",function(){
+//     window.open();
+// })
+
